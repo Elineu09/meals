c4e4b3b018d596823f4597894e29532791e3e3ff
Elineu <elineutomas4@gmail.com>
2025-08-25 14:38:13 +0100

    Meals app - first commit

---
+++ lib/screens/details.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:meals/models/meal.dart';
+import 'package:meals/widgets/meal_detail.dart';
+
+class DetailsScreen extends StatelessWidget {
+  const DetailsScreen({
+    super.key,
+    required this.meal,
+    required this.onToggleFavorite,
+  });
+
+  final Meal meal;
+  final void Function(Meal meal) onToggleFavorite;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(meal.title),
+        actions: [
+          IconButton(
+            onPressed: () {
+              onToggleFavorite(meal);
+            },
+            icon:  Icon(Icons.star),
+          ),
+        ],
+      ),
+      body: MealDetail(meal: meal),
+    );
+  }
+}
